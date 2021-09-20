c39c0f097d53fef90d2285b2b1f481e3c7c50224
Daniel Yanagita <daniel.yanagita@antt.gov.br>
2021-09-19 23:18:13 -0300

    Project Upload

---
+++ src/main/java/com/danielyanagita/repository/FaturaRepository.java
@@ -0,0 +1,16 @@
+package com.danielyanagita.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.danielyanagita.domain.Fatura;
+import com.danielyanagita.domain.Instalacao;
+
+public interface FaturaRepository extends JpaRepository<Fatura, Long> {
+
+	public Optional<Fatura> findByCodigo(String codigo);
+	public List<Fatura> findByInstalacao(Instalacao instalacao);
+	
+}
