2dae89d873084c774cdd1fd970ddbab1f020c820
Daniel Yanagita <daniel.yanagita@antt.gov.br>
2021-09-20 00:05:25 -0300

    Project settings

--- src/main/java/com/danielyanagita/DemoDanielYanagitaApplication.java
+++ src/main/java/com/danielyanagita/DemoDanielYanagitaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoAcmeApApplication {
+public class DemoDanielYanagitaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoAcmeApApplication.class, args);
+		SpringApplication.run(DemoDanielYanagitaApplication.class, args);
 	}
 
 }
